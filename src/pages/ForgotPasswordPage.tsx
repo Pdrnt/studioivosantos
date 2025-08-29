5155cb253053bceeb665ee3f03c0b1c6243b228d
Pedro <pedrocarvalholima2015@gmail.com>
2025-08-29 13:48:35 -0300

    Forgot Password retornar

--- src/pages/ForgotPasswordPage.tsx
+++ src/pages/ForgotPasswordPage.tsx
@@ -139,7 +139,7 @@ export const ForgotPasswordPage: React.FC = () => {
                     to="/login"
                     className="text-sm font-medium text-[#E3A872] hover:text-[#D89860]"
                   >
-                    Voltar para o Login
+                    Voltar para o login.
                   </Link>
                 </div>
               </form>
